9ef5f1370eccd9ea304aac8396d959d8f389b6fd
Paal Kobbeltvedt <pako@computas.com>
2026-04-22 21:41:38 +0200

    Add python script

---
+++ post_tweet.py
@@ -0,0 +1,12 @@
+import tweepy
+import os
+
+client = tweepy.Client(
+    consumer_key=os.environ["TWITTER_API_KEY"],
+    consumer_secret=os.environ["TWITTER_API_SECRET"],
+    access_token=os.environ["TWITTER_ACCESS_TOKEN"],
+    access_token_secret=os.environ["TWITTER_ACCESS_SECRET"],
+)
+
+client.create_tweet(text="Hello from my bot!")
+print("Tweet posted successfully!")
