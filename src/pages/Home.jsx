abfd478716e89b00406d9e4823541f6baa2a38d4
Paal Kobbeltvedt <pako@computas.com>
2026-05-01 22:12:49 +0200

    improve SEO

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import Logo from '../components/Logo'
 
 export default function Home() {
-  useEffect(() => { document.title = 'Brostein' }, [])
+  useEffect(() => { document.title = 'Brostein — Bar i Oslo' }, [])
   return (
     <main className="hero">
       <Logo />
